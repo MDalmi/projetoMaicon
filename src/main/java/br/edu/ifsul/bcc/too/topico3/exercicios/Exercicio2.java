6cc7ef4e8068a7baa2890ccbe290b1a351184f64
Maicon <104467855+MDalmi@users.noreply.github.com>
2023-08-21 11:04:02 -0300

    Add files via upload

--- src/main/java/br/edu/ifsul/bcc/too/topico3/exercicios/Exercicio2.java
+++ src/main/java/br/edu/ifsul/bcc/too/topico3/exercicios/Exercicio2.java
@@ -1,6 +1,6 @@
-
 package br.edu.ifsul.bcc.too.topico3.exercicios;
 
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 /**
@@ -8,32 +8,76 @@ import javax.swing.JOptionPane;
  * @author Prof. Telmo Júnior
  */
 public class Exercicio2 {
-    
-    static  int[][] rgDados1 = new int[3][3];
-    
-    static  int[][] rgDados2 = new int[3][3];
-        
-    static  int[][] rgDados3 = new int[3][3];
-    
+
+    static int[][] rgDados1 = new int[3][3];
+
+    static int[][] rgDados2 = new int[3][3];
+
+    static int[][] rgDados3 = new int[3][3];
+
     public static void main(String[] args) {
-        
+
         /*
             1) Com base no código fonte produzido no exercicio 1, 
         implemente um tratamento de exceção para tratar possível erro na conversão de 
                string para integer no comando "Integer.parseInt(sc.next())".
         
                Documentação: https://www.devmedia.com.br/blocos-try-catch/7339
-        */
         
-        for (int linha=0; linha < rgDados1.length; linha++){
+       
+         */
+        Scanner sc = new Scanner(System.in);
 
-             for(int coluna=0; coluna < rgDados1[linha].length; coluna++){
-              
-                 //....
+        for (int linha = 0; linha < rgDados1.length; linha++) {
 
-             }                        
+            for (int coluna = 0; coluna < rgDados1[linha].length; coluna++) {
+
+                boolean convertido = true;
+                do{
+                    try{
+                        System.out.println("Digite um valor para a posicao ["+linha+"]["+coluna+"] na M1:");
+                        if(sc.hasNext()){
+                            rgDados1[linha][coluna] = Integer.parseInt(sc.next());
+                        }
+                        convertido = true;
+                        
+                    }catch(Exception e){
+                        convertido = false;
+                    }
+                    
+                }while(convertido == false);
+                
+                do{
+                    try{
+                        System.out.println("Digite um valor para a posicao ["+linha+"]["+coluna+"] na M2:");
+                        if(sc.hasNext()){
+                            rgDados2[linha][coluna] = Integer.parseInt(sc.next());
+                        }
+                        convertido = true;
+                        
+                    }catch(Exception e){
+                        convertido = false;
+                    }
+                    
+                }while(convertido == false);
+                rgDados3[linha][coluna] = rgDados1[linha][coluna] + rgDados2[linha][coluna];
+
+            }
+
         }
-        
+
+        for (int linha = 0; linha < rgDados3.length; linha++) {
+
+            for (int coluna = 0; coluna < rgDados3[linha].length; coluna++) {
+
+                System.out.print(rgDados3[linha][coluna] + " ");
+
+            }
+            System.out.println("\n");
+        }
+
     }
-    
+
 }
+
+
--- src/main/java/br/edu/ifsul/bcc/too/topico3/exercicios/Exercicio4.java
+++ src/main/java/br/edu/ifsul/bcc/too/topico3/exercicios/Exercicio4.java
@@ -17,14 +17,28 @@ public class Exercicio4 {
         Comando para execução (a partir do diretorio java): java br/edu/ifsul/bcc/too/base/topico3/exercicios/Exercicio4
      */
     public Exercicio4() {
-
     }
 
     public static void main(String[] args) {
+        
+      
+       
+        System.out.println("Quantidade de parâmetros/argumentos: "+args.length);
+        
+      
+        
+        
 
         if (args.length == 2) {
+            System.out.println("Primeiro Parâmetro: " + args[0]);
+            System.out.println("Segundo Parâmetro: " + args[1]);
+            Float p1 = Float.parseFloat(args[0]);
+            Float p2 = Float.parseFloat(args[1]);
+            Float m = (p1 + p2) / 2f;
             
-           // System.out.println("Parametro/argumento recebido pela funcao main: " + args[2]);
+            System.out.println("Media: "+m);
+            
+            System.out.println("Parametro/argumento recebido pela funcao main: " + args[2]);
         } else {
             System.out.println("Não recebeu Parametro/argumento recebido pela funcao main.");
         }
